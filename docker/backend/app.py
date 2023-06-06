8c29f0a97e32d1c2193f5deb15b6320fe699b494
Tobi <olajumoketobi01@gmail.com>
2023-06-06 23:43:45 +0100

    add docker material and demo

--- docker/backend/app.py
+++ docker/backend/app.py
@@ -7,4 +7,4 @@ def hello_world():
     return {'message': 'Hello, Senior Devops Engineers minus RUYI!'}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.
+    app.run(debug=True, host='0.0.0.0')
